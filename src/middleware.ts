edaf856f1fe58b73599e3ad1e6f002dda149bb38
pixelsock <nick@stump.works>
2024-10-08 20:10:06 -0400

    Changed Middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,18 +13,11 @@ const isPublicRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth().protect()
+  // If the route is public, no need to protect it
+  if (isPublicRoute(req)) {
+    return
   }
 
+  // For non-public routes, enforce authentication
+  auth().protect()
 })
-
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
